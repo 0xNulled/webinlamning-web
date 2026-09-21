a7d474f01d41a640f06c1c9c5dfbe455b803aa5e
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-09-21 13:45:10 +0200

    BandCard tar array input

--- src/components/BandCard.jsx
+++ src/components/BandCard.jsx
@@ -1,13 +1,37 @@
 import { useState } from "react";
 
-function BandCard({ AlbumName, Artist, ListeningStatus }){
+const AlbumsFetched = [
+    { id: 1, AlbumName: "III" , Artist: "Portrait decay" , ListeningStatus: true },
+    { id: 2, AlbumName: "WhirlWind", Artist: "Demonic Science", ListeningStatus: false },
+    { id: 3, AlbumName: "WhirlWind", Artist: "Demonic Science", ListeningStatus: false }
+
+]
+
+function BandCard(){
+    const [album, setAlbum] = useState(AlbumsFetched);
+    //const [status, setStatus] = useState();
+
+    function handleClick(name) {
+        console.log(name)
+        setAlbum(
+            album.map((a) => 
+                a.AlbumName == name ? {...a, ListeningStatus: !a.ListeningStatus}  : a 
+            )
+        );
+    }
+
     return (
-        <div className="Card">
-            <article>
-                <h3> {AlbumName} </h3>
-                <p> {Artist} </p>
-                <p> {ListeningStatus} </p>
-            </article>
+        <div className="CardHolder">
+            {album.map((a) => (
+                <div className="Card" key={a.AlbumName}>
+                    <article>
+                    <h3> {a.AlbumName} </h3>
+                    <p> {a.Artist} </p>
+                    <p>{(a.ListeningStatus).toString()} </p>
+                    <button onClick={() => handleClick(a.AlbumName)}> change </button>
+                    </article>
+                </div> 
+            ))}    
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,18 @@ import Header from './components/Header.jsx'
 import SearchBox from './components/SearchBox.jsx'
 import BandCard from './components/BandCard.jsx'
 
+/*const AlbumsFetched = [ 
+  { BandCard: { AlbumName: "III" , Artist: "Portrait decay" , ListeningStatus: "true" }},
+  { BandCard: { AlbumName: "WhirlWind", Artist: "Demonic Science", ListeningStatus: "false "}}
+]*/
+
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
       <div className="MainBox">
         <Header />
         <SearchBox />
-        <BandCard AlbumName={"test"} Artist={"Portrait decay"} ListeningStatus={"true"}/>
-        <BandCard AlbumName={"test"} Artist={"Portrait decay"} ListeningStatus={"true"}/>
+        <BandCard />
       </div>
   </StrictMode>,
 )
