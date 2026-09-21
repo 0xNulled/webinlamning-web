623d134dd4e236e6e801a293e56b291084574695
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-09-21 13:47:42 +0200

    Ändrar Band.listeningstatus baserat på id istället för namn för att stoppa samma namn konflikter (samt tog bort kommenterad kod)

--- src/components/BandCard.jsx
+++ src/components/BandCard.jsx
@@ -3,19 +3,17 @@ import { useState } from "react";
 const AlbumsFetched = [
     { id: 1, AlbumName: "III" , Artist: "Portrait decay" , ListeningStatus: true },
     { id: 2, AlbumName: "WhirlWind", Artist: "Demonic Science", ListeningStatus: false },
-    { id: 3, AlbumName: "WhirlWind", Artist: "Demonic Science", ListeningStatus: false }
+    { id: 3, AlbumName: "Hivemind Narcosis", Artist: "Solar Witch", ListeningStatus: false }
 
 ]
 
 function BandCard(){
     const [album, setAlbum] = useState(AlbumsFetched);
-    //const [status, setStatus] = useState();
 
-    function handleClick(name) {
-        console.log(name)
+    function handleClick(id) {
         setAlbum(
             album.map((a) => 
-                a.AlbumName == name ? {...a, ListeningStatus: !a.ListeningStatus}  : a 
+                a.id == id ? {...a, ListeningStatus: !a.ListeningStatus}  : a 
             )
         );
     }
@@ -28,7 +26,7 @@ function BandCard(){
                     <h3> {a.AlbumName} </h3>
                     <p> {a.Artist} </p>
                     <p>{(a.ListeningStatus).toString()} </p>
-                    <button onClick={() => handleClick(a.AlbumName)}> change </button>
+                    <button onClick={() => handleClick(a.id)}> change </button>
                     </article>
                 </div> 
             ))}    
