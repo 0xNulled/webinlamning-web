358606a1192df8c38a5f2b35049e82ecbfb0ec8b
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-09-22 16:32:53 +0200

    Funktionalitet lades till för att ladda up albumbilder till bandcards, samt att de bilder visas

--- src/components/BandCard.jsx
+++ src/components/BandCard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { ChangeEvent} from 'react';
 
 
 /*const AlbumsFetched = [
@@ -7,27 +8,48 @@ import { useEffect, useState } from "react";
     { id: 3, AlbumName: "Hivemind Narcosis", Artist: "Thantifaxath", ListeningStatus: false }
 ]*/
 
-function BandCard(){
+function BandCard({ onUpload }){
     const [album, setAlbum] = useState([]);
     const [err, setErr] = useState(true);
     const [loading, setLoading] = useState(true)
-    const API_URL = "http://localhost:5245/api/Album"
+    const BAND_API_URL = "http://localhost:5245/api/Album"
+    const IMAGE_API_URL = "http://localhost:5245"
 
     useEffect(() => {
-        fetch(API_URL)
+        fetch(BAND_API_URL)
         .then((res) => res.json())
         .then((data) => setAlbum(data))
-        .catch(() => setErr(`Could not load ${API_URL}`))
+        .catch(() => setErr(`Could not load ${BAND_API_URL}`))
         .finally(() => setLoading(false))
     },[]);
 
-    async function handleClick(id) {
+    function handleImageUpload(e, id) {
+        const file = e.target.files[0];
+        if (!file) {
+            return;
+        }
+
+        e.target.value = "";
+        const formData = new FormData();
+
+        formData.append("file", file);
+
+        fetch((`${BAND_API_URL}/${id}/image`),{
+            method: "POST",
+            body: formData
+        })
+        //.then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error(error));
+    }
+
+    async function changeListeningStatus(id) {
         
         const fetchedAlbum = album.find((a) => a.id == id);
         if (!fetchedAlbum) return;
 
         try {
-            const res = await fetch(`${API_URL}/${id}`, {
+            const res = await fetch(`${BAND_API_URL}/${id}`, {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ ...fetchedAlbum, listeningStatus: !fetchedAlbum.listeningStatus }),
@@ -54,10 +76,17 @@ function BandCard(){
             {album.map((a) => (
                 <div className="Card" key={a.id}>
                     <article>
+                        <img src={`${IMAGE_API_URL}${a.imageURL}`}></img>
                     <h3> {a.albumName} </h3>
                     <p> {a.artist} </p>
                     <p>{(a.listeningStatus).toString()} </p>
-                    <button onClick={() => handleClick(a.id)}> change </button>
+                    <button onClick={() => changeListeningStatus(a.id)}> change </button>
+                    <label onClick={((e) => e.stopPropagation())}>
+                        <input 
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => handleImageUpload(e, a.id)}/>
+                    </label>
                     </article>
                 </div> 
             ))}    
