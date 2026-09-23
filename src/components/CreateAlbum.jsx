edcaf284a8c862266ef5d0973a2590cb5c38263f
Joel Lindström <test@fedora.com>
2026-09-23 21:23:05 +0200

    Lade till funktionalitet i UI att skapa ett nytt album

--- src/components/CreateAlbum.jsx
+++ src/components/CreateAlbum.jsx
@@ -2,15 +2,38 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react";
 
 function CreateAlbumBox() {
+    const [err, setErr] = useState();
+    const [albumName, setAlbumName] = useState("");
+    const [artist, setArtist] = useState("");
+    const [listeningStatus, setListeningStatus] = useState(false);
+    const API_URL = "http://localhost:5245/api/album/addAlbum";
+
+    async function CreateAlbum(e) {
+        e.preventDefault()
+        const album = { id: -1, albumName: albumName, artist: artist, listeningStatus: listeningStatus, imageURL: null }
+        
+        try {
+            const res = await fetch(API_URL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(album)
+                })
+
+                window.location.reload();
+            } catch (error) {
+            setErr(error.message)
+        }
+    }
+
     return (
         <div className="CreateAlbumBox">
-            <form>
-            <input type="text" placeholder="Album namn" />
+            <form onSubmit={CreateAlbum}>
+            <input type="text" id="albumName" placeholder="Album namn" value={albumName} onChange={(e) => setAlbumName(e.target.value)} />
             <br />
-            <input type="text" placeholder="Artist namn" />
+            <input type="text" id="artist" placeholder="Artist namn" value={artist} onChange={(e) => setArtist(e.target.value)}/>
             <br />
             <label> Har du lyssnat igenom detta band? </label>
-            <input type="checkbox" placeholder="Artist namn" /> 
+            <input type="checkbox" id="listeningStatus" placeholder="Artist namn" checked={listeningStatus} onChange={(e) => setListeningStatus(e.target.checked)}/> 
             <br />
             <input type="submit" value="Spara album" />
             </form> 
